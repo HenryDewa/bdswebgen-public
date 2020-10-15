7c6f97702874ea63da14c022657005ca61b3221a
henry <henry.finasmart@gmail.com>
2020-10-15 16:41:13 +0700

    init commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>BDS WEB GEN 2</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Basic Styles -->
+  <!-- <link rel="stylesheet" type="text/css" media="screen" href="assets/css/tailwind.min.css"> -->
+  <!-- <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css" /> -->
+  <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"> -->
+  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+   -->
+  <link rel="shortcut icon" href="./favicon.ico" />
+  <link rel="apple-touch-icon" sizes="76x76" href="./apple-icon.png" />
+<link rel="stylesheet" href="styles.1ffdab2ff9aeac76b56e.css"></head>
+
+<body style="background-color: #F0F0F0;">
+  <app-root></app-root>
+<script src="runtime-es2015.361ed2e0f3b26f39f32a.js" type="module"></script><script src="runtime-es5.361ed2e0f3b26f39f32a.js" nomodule defer></script><script src="polyfills-es5.d14284bc9937dc71fac9.js" nomodule defer></script><script src="polyfills-es2015.c22deca97da1aafbcba8.js" type="module"></script><script src="main-es2015.4e3880d729013580559c.js" type="module"></script><script src="main-es5.4e3880d729013580559c.js" nomodule defer></script></body>
+
+</html>
